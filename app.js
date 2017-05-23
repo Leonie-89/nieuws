d0fa3f30ed9dcacfa00929bf4aedec165aa24244
Leonie-89 <Leonievandermeer89@gmail.com>
2017-05-23 22:12:45 +0200

    website aangepast

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var app = express();
 app.set('nieuwsFile', require('./config/nieuws.json'));
 app.set('categorieenFile', require('./config/categorieen.json'));
 
-
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
